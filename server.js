2405bd3899f660f6589bcfcd2f043e4d0a063a13
temuriee <critstoggle@gmail.com>
2026-02-20 03:43:16 -0800

    finish

--- server.js
+++ server.js
@@ -2,19 +2,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 
-import hobbyRouter from "./routes/hobbyRoutes";
-import mongoose from "mongoose";
-
 dotenv.config();
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("MongoDB Connection Failed Error:", err);
-  });
 
 const app = express();
 const PORT = 4000;
@@ -22,8 +10,14 @@ const PORT = 4000;
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/hobbies", hobbyRouter);
+app.get("/", (req, res) => {
+  res.json({ status: "ok", message: "Server is running" });
+});
+
+app.get("/my-hobby", (req, res) => {
+  res.json({ hobby: "ლაშქრობა და ცხენით სეირნობა" });
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server Listening On http://localhost:${PORT}`);
 });
